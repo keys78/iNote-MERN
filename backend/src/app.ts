1882e1bee8561368006e0274aced79710af3947b
Emmanuel Onyeyaforo <rahzy24@gmail.com>
2023-03-14 10:51:52 +0100

    cors-error fix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
 import morgan from "morgan";
+import cors from "cors"
 import createHttpError, { isHttpError } from "http-errors";
 import notesRoutes from "./routes/notes";
 import boardsRoutes from "./routes/boards";
@@ -9,6 +10,7 @@ import authRoutes from "./routes/auth";
 
 
 const app = express();
+app.use(cors());
 
 app.use(morgan("dev"));
 app.use(express.json());
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,27 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// import '@/styles/globals.css'
+// import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// export default function App({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />
+// }
+
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'next-themes';
+import { Provider } from 'react-redux'
+import { store } from '../network/store'
+
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider attribute='class'>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </ThemeProvider>
+
+  )
 }
+
+export default MyApp
+
