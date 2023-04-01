8bdf7f05af0826ee62328720565e26553133b50a
Emmanuel Onyeyaforo <rahzy24@gmail.com>
2023-04-01 12:53:13 +0100

    review-UI build

--- frontendd/src/components/User/ReviewCard.tsx
+++ frontendd/src/components/User/ReviewCard.tsx
@@ -1,60 +1,70 @@
 /* eslint-disable @next/next/no-img-element */
-import { Heart, Star } from 'phosphor-react'
+import { Star } from 'phosphor-react'
 import React from 'react'
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css/skyblue';
+import '@splidejs/react-splide/css/sea-green';
+import '@splidejs/react-splide/css/core';
+import '@splidejs/react-splide/css';
 
 
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-import "swiper/css/free-mode";
 
 
-// import required modules
-import { Autoplay, Pagination, Navigation, FreeMode } from "swiper";
+
 
 const ReviewCard = () => {
-
     const sampleData = [
+        { username: 'Volcano', role: 'user', photo: 'https://source.unsplash.com/random/300x200', star: 3, comment: 'lorem, ipsum dolor sit amet consectetur ' },
+        { username: 'Volcano', role: 'user', photo: 'https://source.unsplash.com/random/300x200', star: 3, comment: 'lorem, ipsum dolor sit amet consectetur ' },
+        { username: 'Volcano', role: 'user', photo: 'https://source.unsplash.com/random/300x200', star: 3, comment: 'lorem, ipsum dolor sit amet consectetur ' },
+        { username: 'Volcano', role: 'user', photo: 'https://source.unsplash.com/random/300x200', star: 3, comment: 'lorem, ipsum dolor sit amet consectetur ' },
+        { username: 'Volcano', role: 'user', photo: 'https://source.unsplash.com/random/300x200', star: 3, comment: 'lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab praesentium minus quaerat cumque ut, repellat maxime enim a ipsa autem accusamus quos modi sunt?' },
         { username: 'Volcano', role: 'user', photo: 'https://source.unsplash.com/random/300x200', star: 3, comment: 'lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab praesentium minus quaerat cumque ut, repellat maxime enim a ipsa autem accusamus quos modi sunt?' },
         { username: 'Volcano', role: 'user', photo: 'https://source.unsplash.com/random/300x200', star: 3, comment: 'lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab praesentium minus quaerat cumque ut, repellat maxime enim a ipsa autem accusamus quos modi sunt?' },
     ]
 
     return (
-        <div className='flex space-x-3'>
-            <Swiper
-                modules={[Navigation, Pagination]}
-                spaceBetween={1}
-                slidesPerView={1}
-                navigation
-                pagination={{ clickable: true }}
-                scrollbar={{ draggable: true }}
-                onSwiper={(swiper) => console.log(swiper)}
-                onSlideChange={() => console.log('slide change')}
-            >
-                {sampleData.map((val: any, i: number) =>
-                    <SwiperSlide key={i}>
-                        <div className='review-card' key={i}>
-                            <div>
-                                <img src={val?.photo} alt='' />
-                                <div className='user-details'>
-                                    <h1>{val?.username}</h1>
-                                    <h2>{val?.role}</h2>
-                                    <span>
-                                        <Star key={val} size={20} color="#4a4db0" weight="fill" />
-                                        <Star key={val} size={20} color="#4a4db0" weight="fill" />
-                                        <Star key={val} size={20} color="#4a4db0" weight="fill" />
-                                    </span>
-                                </div>
-                            </div>
-                            <article>{val?.comment}</article>
-                        </div>
-                    </SwiperSlide>
-                )}
-               
-            </Swiper>
+        <Splide
+            options={{
+                rewind: true,
+                gap: '2px',
+                autoplay: true,
+                pauseOnHover: true,
+                autoplay: true,
+                interval: 5000,
+                speed: 5000,
+                perPage: 1,
+                autoHeight: true,
+                autoWidth: true,
+                arrows: false,
+                isNavigation: false,
 
-        </div>
+            }}
+            aria-label="iNote reviews"
+            className='max-w-[510px]'
+        >
+
+            {sampleData.map((val: any, i: number) =>
+                <SplideSlide key={i}>
+                    <div className='review-card my-3' key={i}>
+                        <div>
+                            <img src={val?.photo} alt='' />
+                            <div className='user-details'>
+                                <h1>{val?.username}</h1>
+                                <h2>{val?.role}</h2>
+                                <span>
+                                    <Star key={val} size={20} color="#4a4db0" weight="fill" />
+                                    <Star key={val} size={20} color="#4a4db0" weight="fill" />
+                                    <Star key={val} size={20} color="#4a4db0" weight="fill" />
+                                </span>
+                            </div>
+                        </div>
+                        <article>{val?.comment}</article>
+                    </div>
+                </SplideSlide>
+            )}
+
+        </Splide>
     )
 }
 
@@ -79,4 +89,3 @@ export default ReviewCard;
 //         <article>{val?.comment}</article>
 //     </div>
 // )}
-
